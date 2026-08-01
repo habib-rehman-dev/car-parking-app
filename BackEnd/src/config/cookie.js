e4ebf590155b446ede4e956c3cfb18847515e2e2
habib-rehman-dev <contact.habib.dev@gmail.com>
2026-08-01 15:35:25 -0400

    ensure the db connection

--- BackEnd/src/config/cookie.js
+++ BackEnd/src/config/cookie.js
@@ -1,12 +1,11 @@
 import cookieParser from "cookie-parser";
 import "./env.js";
-
 // src/config/cookie.js (or wherever cookieOptions is defined)
 const isProduction = process.env.NODE_ENV === "production";
 
-export const cookieParser = {
+export default cookieParser({
   httpOnly: true, // Prevents XSS attacks (JavaScript cannot read cookie)
   secure: isProduction, // MUST be true in production (requires HTTPS)
   sameSite: isProduction ? "none" : "lax", // MUST be "none" for cross-origin frontend/backend!
   path: "/",
-};
+})
